b142eaf20f2a50cc2d80bcb1866fa2cec8a001eb
Casper Turesson <casperture@gmail.com>
2023-07-12 10:40:51 +0200

    Application now creates a window on startup

--- win32_tetris.c
+++ win32_tetris.c
@@ -1,7 +1,44 @@
-#include <stdio.h>
+#include <Windows.h>
 
-int main() {
-	printf("Hello World!\n");
+static int g_isRunning;
 
-	return 0;
+static LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_DESTROY:
+        case WM_CLOSE: {
+            g_isRunning = 0;
+        } break;
+    }
+
+    return DefWindowProc(window, message, wParam, lParam);
+}
+
+int CALLBACK WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ LPSTR cmdLine, _In_ int showCmd) {
+    WNDCLASSA windowClass = {
+        .style = CS_HREDRAW | CS_VREDRAW,
+        .lpfnWndProc = WndProc,
+        .hInstance = instance,
+        .lpszClassName = "tetris window class"
+        // hIcon, hCursor
+    };
+    if (!RegisterClass(&windowClass)) {
+        return 1;
+    }
+
+    HWND window = CreateWindowEx(0, windowClass.lpszClassName, L"Tetris", WS_OVERLAPPEDWINDOW | WS_VISIBLE, \
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, instance, NULL);
+    if (!window) {
+        return 1;
+    }
+
+    g_isRunning = 1;
+    while (g_isRunning) {
+        MSG message;
+        while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&message);
+            DispatchMessageA(&message);
+        }
+    }
+
+    return 0;
 }
