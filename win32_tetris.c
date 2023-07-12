810111ed3ed9a038d942a6c140a43add2c0b06ef
Casper Turesson <casperture@gmail.com>
2023-07-12 12:12:46 +0200

    Window now displays a bitmap (solid colour)

--- win32_tetris.c
+++ win32_tetris.c
@@ -1,7 +1,57 @@
 #include <Windows.h>
 #include "tetris_types.h"
 
+typedef struct win32_bitmap {
+    BITMAPINFO info;
+    void* memory;
+    i32 width;
+    i32 height;
+    i32 pitch;
+} win32_bitmap;
+
+typedef struct ivec2 {
+    i32 x;
+    i32 y;
+} ivec2;
+
 static b32 g_isRunning;
+static win32_bitmap g_graphicsBuffer;
+
+static ivec2 GetWindowDimensions(HWND window) {
+    RECT clientRect;
+    GetClientRect(window, &clientRect);
+    return (ivec2){ clientRect.right - clientRect.left, clientRect.bottom - clientRect.top };
+}
+
+#define BYTES_PER_PIXEL 4
+static void InitBitmap(win32_bitmap* bitmap, i32 width, i32 height) {
+    if (bitmap->memory != NULL) {
+        VirtualFree(bitmap->memory, 0, MEM_RELEASE);
+    }
+
+    bitmap->width  = width;
+    bitmap->height = height;
+    bitmap->pitch  = width * BYTES_PER_PIXEL;
+
+    bitmap->info.bmiHeader = (BITMAPINFOHEADER){
+        .biSize = sizeof(bitmap->info.bmiHeader),
+        .biWidth = width,
+        .biHeight = height,
+        .biPlanes = 1,
+        .biBitCount = BYTES_PER_PIXEL * 8,
+        .biCompression = BI_RGB
+    };
+
+    i32 memorySize = width * height * BYTES_PER_PIXEL;
+    bitmap->memory = VirtualAlloc(NULL, memorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+}
+#undef BYTES_PER_PIXEL
+
+static void DisplayBitmapInWindow(const win32_bitmap* bitmap, HDC deviceContext, i32 windowWidth, i32 windowHeight) {
+    SetStretchBltMode(deviceContext, STRETCH_DELETESCANS);
+    StretchDIBits(deviceContext, 0, 0, windowWidth, windowHeight, \
+        0, 0, bitmap->width, bitmap->height, bitmap->memory, &bitmap->info, DIB_RGB_COLORS, SRCCOPY);
+}
 
 static LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
@@ -9,9 +59,28 @@ static LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM
         case WM_CLOSE: {
             g_isRunning = false;
         } break;
+        case WM_PAINT: {
+            PAINTSTRUCT paint;
+            HDC deviceContext = BeginPaint(window, &paint);
+            ivec2 windowDimensions = GetWindowDimensions(window);
+            DisplayBitmapInWindow(&g_graphicsBuffer, deviceContext, windowDimensions.x, windowDimensions.y);
+            EndPaint(window, &paint);
+        } break;
     }
 
     return DefWindowProc(window, message, wParam, lParam);
+}
+
+#define RGB(r, g, b) ((r) << 16) | ((g) << 8) | (b)
+static void TEST_FillBitmap(win32_bitmap* bitmap) {
+    u8* row = bitmap->memory;
+    for (i32 y = 0; y < bitmap->height; ++y) {
+        u32* pixel = row;
+        for (i32 x = 0; x < bitmap->width; ++x) { 
+            *pixel++ = RGB(255, 0, 0);
+        }
+        row += bitmap->pitch;
+    }
 }
 
 int CALLBACK WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ LPSTR cmdLine, _In_ int showCmd) {
@@ -31,6 +100,10 @@ int CALLBACK WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _
     if (!window) {
         return 1;
     }
+
+    // Change size later
+    InitBitmap(&g_graphicsBuffer, 480, 270);
+    TEST_FillBitmap(&g_graphicsBuffer);
 
     g_isRunning = true;
     while (g_isRunning) {
