c7b7b8bef0275d455564669e8a88408610d5df37
Casper Turesson <casperture@gmail.com>
2023-08-11 11:44:56 +0200

    Improved BMP code (opacity support etc.)

--- tetris.c
+++ tetris.c
@@ -61,39 +61,41 @@ static bitmap_buffer LoadBMP(const char* filePath) {
     }
 
     bitmap_header* header = (bitmap_header*)contents;
-    Assert(header->bitsPerPixel == 32);
+
+    if ((header->fileType[0] != 'B' || header->fileType[1] != 'M') || \
+        (header->bitsPerPixel != 32)) {
+        return (bitmap_buffer) { 0 };
+    }
 
     bitmap_buffer bitmap = { 
-        .memory = (u8*)contents + header->dataOffset,
-        .width  = header->width,
-        .height = header->height,
+        .memory        = (u8*)contents + header->dataOffset,
+        .width         = header->width,
+        .height        = header->height,
         .bytesPerPixel = 4,
-        .pitch  = header->width * 4
+        .pitch         = header->width * 4
     };
 
     u32 bitShiftRed;
     u32 bitShiftGreen;
     u32 bitShiftBlue;
     u32 bitShiftAlpha;
-    if (header->compression == 0) { // BI_RGB
-        bitShiftRed   = 0;  // 0x000000FF
-        bitShiftGreen = 8;  // 0x0000FF00
-        bitShiftBlue  = 16; // 0x00FF0000
-        bitShiftAlpha = 24; // 0xFF000000
-    }
-    else if (header->compression == 3 || header->compression == 6) { // BI_BITFIELDS or BI_ALPHABITFIELDS
-        u32 bitmaskRed   = header->bitmaskRed;
-        u32 bitmaskGreen = header->bitmaskGreen;
-        u32 bitmaskBlue  = header->bitmaskBlue;
-        u32 bitmaskAlpha = ~(bitmaskRed | bitmaskGreen | bitmaskBlue);
-
-        bitShiftRed   = GetLeastSignificantSetBitIndex(bitmaskRed);
-        bitShiftGreen = GetLeastSignificantSetBitIndex(bitmaskGreen);
-        bitShiftBlue  = GetLeastSignificantSetBitIndex(bitmaskBlue);
-        bitShiftAlpha = GetLeastSignificantSetBitIndex(bitmaskAlpha);
-    }
-    else { // I can't be bothered with more complex compression lol, and the compression should always be BI_ALPHABITFIELDS regardless
-        return (bitmap_buffer) { 0 };
+    switch (header->compression) {
+        case 0: { // BI_RGB
+            bitShiftRed   = 16; // 0x00FF0000
+            bitShiftGreen = 8;  // 0x0000FF00
+            bitShiftBlue  = 0;  // 0x000000FF
+            bitShiftAlpha = 24; // 0xFF000000
+        } break;
+        case 3:   // BI_BITFIELDS
+        case 6: { // BI_ALPHABITFIELDS
+            bitShiftRed   = GetLeastSignificantSetBitIndex(header->bitmaskRed);
+            bitShiftGreen = GetLeastSignificantSetBitIndex(header->bitmaskGreen);
+            bitShiftBlue  = GetLeastSignificantSetBitIndex(header->bitmaskBlue);
+            bitShiftAlpha = GetLeastSignificantSetBitIndex(~(header->bitmaskRed | header->bitmaskGreen | header->bitmaskBlue));
+        } break;
+        default: {
+            return (bitmap_buffer){ 0 };
+        } break;
     }
 
     u32* pixels = bitmap.memory;
@@ -114,21 +116,45 @@ static void TEST_DrawBitmap(bitmap_buffer* graphicsBuffer, bitmap_buffer* bitmap
     i32 xMax = Min(x + bitmap->width, graphicsBuffer->width);
     i32 yMax = Min(y + bitmap->height, graphicsBuffer->height);
 
-    u8* row = (u8*)graphicsBuffer->memory + y * graphicsBuffer->pitch + x * graphicsBuffer->bytesPerPixel;
-    u32* source = bitmap->memory;
+    i32 xOffset = x < 0 ? -x : 0;
+    i32 yOffset = y < 0 ? -y : 0;
+
+    u8* rowDest   = (u8*)graphicsBuffer->memory + yMin * graphicsBuffer->pitch + xMin * graphicsBuffer->bytesPerPixel;
+    u8* rowSource = (u8*)bitmap->memory + yOffset * bitmap->pitch + xOffset * bitmap->bytesPerPixel;
     for (i32 y = yMin; y < yMax; ++y) {
-        u32* pixel = row;
+        u32* dest   = rowDest;
+        u32* source = rowSource;
         for (i32 x = xMin; x < xMax; ++x) {
-            *pixel++ = *source++;
+            u32 sa = ((u8*)source)[3];
+            u32 sr = ((u8*)source)[2];
+            u32 sg = ((u8*)source)[1];
+            u32 sb = ((u8*)source)[0];
+
+            u32 dr = ((u8*)dest)[2];
+            u32 dg = ((u8*)dest)[1];
+            u32 db = ((u8*)dest)[0];
+
+            f32 t = sa / 255.0f;
+            u32 r = (1.0f - t) * dr + t * sr;
+            u32 g = (1.0f - t) * dg + t * sg;
+            u32 b = (1.0f - t) * db + t * sb;
+
+            *dest = RGBToU32(r, g, b);
+
+            ++dest;
+            ++source;
         }
-        row += graphicsBuffer->pitch;
+        rowDest   += graphicsBuffer->pitch;
+        rowSource += bitmap->pitch;
     }
 }
 
-static bitmap_buffer g_testBitmap;
+static bitmap_buffer g_testBitmap1;
+static bitmap_buffer g_testBitmap2;
 
 void OnStartup(void) {
-    g_testBitmap = LoadBMP("assets/OpacityTest.bmp");
+    g_testBitmap1 = LoadBMP("assets/OpacityTest.bmp");
+    g_testBitmap2 = LoadBMP("assets/opacity_test.bmp");
 }
 
 void Update(bitmap_buffer* graphicsBuffer, sound_buffer* soundBuffer, keyboard_state* keyboardState, f32 deltaTime) {
@@ -152,7 +178,8 @@ void Update(bitmap_buffer* graphicsBuffer, sound_buffer* soundBuffer, keyboard_s
         }
     }
 
-    TEST_DrawBitmap(graphicsBuffer, &g_testBitmap, 0, 0);
+    TEST_DrawBitmap(graphicsBuffer, &g_testBitmap1, 50, 50);
+    TEST_DrawBitmap(graphicsBuffer, &g_testBitmap2, g_testBitmap1.width + 50, 50);
 
 #if 0
     static f32 tSine = 0.0f;
